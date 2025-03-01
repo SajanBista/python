f277fe6797ae541da39d3c9ad9294e0235f08a0b
Sajan Bista <162802857+SajanBista@users.noreply.github.com>
2025-03-01 23:04:42 +0545

    Create scipy.py

---
+++ scipy.py
@@ -0,0 +1,11 @@
+from scipy.fft import fft, ifft
+import numpy as np
+x = np.array([1.0, 2.0, 1.0, -1.0, 1.5])
+y = fft(x)
+y
+array([ 4.5       +0.j        ,  2.08155948-1.65109876j,
+       -1.83155948+1.60822041j, -1.83155948-1.60822041j,
+        2.08155948+1.65109876j])
+yinv = ifft(y)
+yinv
+array([ 1.0+0.j,  2.0+0.j,  1.0+0.j, -1.0+0.j,  1.5+0.j])
