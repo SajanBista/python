548363e0b9ad918f5e80e623351c456d5a3736d3
sajanbista1030@gmail.com <sajanbista1030@gmail.com>
2025-11-17 20:54:30 +0545

    this is the code for implementing whole linear regression

--- Array/fullLinearRegression.py
+++ Array/fullLinearRegression.py
@@ -81,14 +81,71 @@ if model_choice == 'linear':
     denomenator =0.0
     #for denomemator:
     for x_i in(X_data):
-        x_deviation = x_i - X_mean
+        x_dev = x_i - X_mean
+        x_deviation = x_dev*x_dev
+
 
         denomenator += x_deviation
 
     print(f"the numenator is {numenator} and denomenator = {denomenator}")
 
+    #for slope:
+
+    slope = numenator/denomenator
+
+    print(f" the slope  is {slope}")
+
+    #intercep
+
+    intercept = Y_mean -slope*X_mean
+
+    formatted_slope = f"{slope:.2f}"
+    formatted_intercept =F"{slope:.2f}"
+
+    print(f"the value of intercept is {intercept}")
+
+    #Displaying the regression line equation
+    sign ="+" if intercept >=0 else ""
+    regression_equation = f"{formatted_slope} * {sign} {formatted_intercept}"
+    print(regression_equation)
+    
 
 
+    #Asking User for user if they need visualization
+    visualization = input(print("\nDo you want to visualize your input?(yes/no)"))
+
+    if visualization=='yes':
+        try:
+            import matplotlib.pyplot as plt
+            plt.figure(figsize =(10,6))
+            plt.scatter(X_data,Y_data, color ='blue', label = 'Data Points (X,Y)')
+            X_range =[min(X_data),max(X_data)]
+            Y_pred_range = [intercept +slope * x for x in X_range]
+
+            plt.plot(X_range,Y_pred_range,color='red',
+                     linestyle ='-', linewidth = 2,
+                     label = f'Regression Line :Y = {formatted_slope}*X {sign} {formatted_intercept:.4f}')
+            plt.title('Linear Regression Model Visualization')
+            plt.xlabel('X Data (Independent Variable)')
+            plt.ylabel('Y Data (Dependent Variable)')
+            plt.legend()
+            plt.grid(True)
+            plt.show()
+
+        except ImportError:
+            print("Please install matplotlib'")
+
+    elif visualization == 'no':
+        print("Visualization skipped.")
+        
+    else:
+        print("Invalid choice for visualization. Skipping.")
+
+
+
+            
+            
+                        
 
 
 
