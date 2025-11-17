ef1c021ef90ede24d647153f4e32c14833109a64
sajanbista1030@gmail.com <sajanbista1030@gmail.com>
2025-11-17 09:07:16 +0545

    this is the implementation of regression code and update of work till now

--- Array/fullLinearRegression.py
+++ Array/fullLinearRegression.py
@@ -38,12 +38,66 @@ print(f"X Data (Independent): {X_data}")
 print(f"Y Data (Dependent):   {Y_data}")
 
 
-
+if len(set(X_data))<2:
+    print("critical error! invalid dataset \n All X values are identical. The regression slope cannot be calculated.\n you must insert the datas i.e. different like 5 and 10")
+    exit()
 
 model_choice = input("Which model to run (linear, ridge, logistic or Lasso)? ").lower().strip()
 
 if model_choice == 'linear':
     print(" here we are doing for linear regression")
+
+    X_sum = sum(X_data)
+    Y_sum = sum(Y_data)
+
+    n = num_data
+
+    if n>0:
+        X_mean = X_sum/n
+    else:
+        print("there is no input or no x")
+    if n>0:
+        Y_mean = Y_sum/n
+    else:
+        print("there is no y value might you didn't entered")
+
+    print(f"Mean of x value: {X_mean}")
+    print(f"Mean of y value: {Y_mean}")
+
+
+    #for numenator part:
+    numenator = 0.0
+
+    for  x_i, y_i in zip(X_data, Y_data):
+        x_deviation = x_i - X_mean
+
+
+        y_deviation = y_i -Y_mean
+        product_of_deviation = x_deviation * y_deviation
+
+        numenator += product_of_deviation
+
+    
+    denomenator =0.0
+    #for denomemator:
+    for x_i in(X_data):
+        x_deviation = x_i - X_mean
+
+        denomenator += x_deviation
+
+    print(f"the numenator is {numenator} and denomenator = {denomenator}")
+
+
+
+
+
+
+
+
+
+
+
+
 
 elif model_choice =='ridge':
     print("Here you are doing for rigid regression")
